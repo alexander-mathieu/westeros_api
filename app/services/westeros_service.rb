90991e71a1561b93f900f6947b3b6153c5ef6b6a
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-19 11:46:30 -0600

    Refactor WesterosService

--- app/services/westeros_service.rb
+++ app/services/westeros_service.rb
@@ -1,30 +1,30 @@
 class WesterosService
   def initialize(house)
     @house = house
+    @house_id = house_id
   end
 
   def house_member_search
-    parse_api_call("house/#{house_id}")
+    parse_api_call("house/#{@house_id}")
   end
-  
+
   private
-  attr_reader :house
 
   def house_id
-    results = parse_api_call('house')
-    house = results.detect do |result|
+    houses = parse_api_call('house')
+    house = houses.detect do |result|
       result[:name].downcase == @house
     end
     house[:id]
   end
 
-  def parse_api_call(url, params = {})
-    response = conn.get(url, params)
+  def parse_api_call(uri)
+    response = conn.get(uri)
     JSON.parse(response.body, symbolize_names: true)
   end
 
   def conn
-    Faraday.new(url: 'https://westeros-as-a-service.herokuapp.com/api/v1/') do |f|
+    Faraday.new(url: 'https://westeros-as-a-service.herokuapp.com/api/v1') do |f|
       f.headers['x_api_key'] = ENV['NEW_SUPER_SECRET_KEY']
       f.adapter Faraday.default_adapter
     end
