46ca09e2b62b7cb1ea5aede702be7788c6a2f259
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-19 10:56:45 -0600

    Change instance method naming

--- app/facades/search_index_facade.rb
+++ app/facades/search_index_facade.rb
@@ -5,19 +5,19 @@ class SearchIndexFacade
     @house = house
   end
 
-  def total_results
-    results.count
+  def house_member_count
+    house_member_results.count
   end
 
   def house_members
-    @house_members ||= results.map do |attributes|
+    @house_members ||= house_member_results.map do |attributes|
       HouseMember.new(attributes)
     end
   end
 
   private
 
-  def results
+  def house_member_results
     @results ||= westeros_service.house_member_search
   end
 
