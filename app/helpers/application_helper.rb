624e639352b2febb64105a59b5e56f415574418b
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-19 09:54:52 -0600

    Add helper method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def game_of_thrones_houses
+    [
+      ['Stark', 'stark'],
+      ['Lannister', 'lannister'],
+      ['Targaryen', 'targaryen'],
+      ['Tyrell', 'tyrell'],
+      ['Greyjoy', 'greyjoy']
+    ]
+  end
 end
