04b4e4afd3bd0cb41facd95686afc5dc67b3f65d
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-19 10:07:49 -0600

    Add additional specs for User GOT house search feature

--- spec/features/users/user_can_search_for_house_members_spec.rb
+++ spec/features/users/user_can_search_for_house_members_spec.rb
@@ -1,20 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe 'As a user' do
-  describe 'when I visit the root path' do
+  describe 'when I visit /' do
     it 'I see a dropdown menu and button' do
       visit root_path
 
-      expect(page).to have_field('Select House')
-      expect(page).to have_button('Get Members')
+      expect(page).to have_field(:house)
+      expect(page).to have_button('Search Members')
+    end
+
+    describe "and select 'Greyjoy' from the dropdown menu and click 'Search Members'" do
+      before :each do
+        visit root_path
+
+        select 'Greyjoy', from: :house
+        click_button 'Search Members'
+      end
+
+      it 'my path should be /search' do
+        expect(current_path).to eq(search_path)
+      end
+
+      it "I see 'house=greyjoy' in the parameters" do
+        expect(current_url).to include('house=greyjoy')
+      end
+
+      it "I see a message that says '7 Members'" do
+        expect(page).to have_content('7 Members')
+      end
+
+      it 'I see a list of the 7 members of House Greyjoy, each with a name and ID' do
+
+      end
     end
   end
 end
 
 
 
-# And I select "Greyjoy" from the dropdown
-# And I click on "Get Members"
 # Then my path should be "/search" with "house=greyjoy" in the parameters
 # And I should see a message "7 Members"
 # And I should see a list of the 7 members of House Greyjoy
